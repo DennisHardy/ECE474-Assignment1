8c19fd294f9078f6a7ef239a718279c3fa7b272e
Zac-4 <33640205+Zac-4@users.noreply.github.com>
2018-09-11 23:35:02 -0700

    Implemented SUB

---
+++ Datapath Components/SUB.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+//  
+// Engineer: Zachary Alvarez
+// Module: SUB
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SUB(a, b, sum);
+    parameter DATAWIDTH = 10;
+    input [DATAWIDTH-1:0] a, b;
+    output reg [DATAWIDTH-1:0] sum;
+    always@(a,b) begin
+        sum <= a - b;
+    end
+endmodule
