13198e2d7dfd3df75b0f1899b81b0bbe743efbd9
Dennis Hardy <DennisHardy@email.arizona.edu>
2018-09-25 20:31:26 -0700

    Circuit 3 complete

--- Behavioral netlists/circuit3.v
+++ Behavioral netlists/circuit3.v
@@ -6,23 +6,23 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Circuit1 (a,b,c,d,e,f,g,h,sa,avg);
-input [15:0] a,b,c,d,e,f,g,h;
-input [7:0] sa;
-output reg [15:0] avg;
-
-wire [31:0] l00, l01, l02, l03, l10, l11, l2, l2div2, l2div4, l2div8;
-ADD ADD1(1,b,l00);
-ADD ADD2(c,d,l01);
-ADD ADD3(e,f,l02);
-ADD ADD4(g,h,l03);
-ADD ADD5(l00,l01,l10);
-ADD ADD6(l02,l03,l11);
-ADD ADD7(l02,l11,l2);
-SHR SHR1(l2,l2div2,sa);
-SHR SHR2(l2div2,l2div4,sa);
-SHR SHR3(l2div4,l2div8,sa);
-    always @(l2div8) begin
-    avg <= l2div8;
-    end
+module Circuit3 (a,b,c,d,e,f,g,h,sa,avg, Clk, Rst);
+    input Clk, Rst;
+    input [15:0] a,b,c,d,e,f,g,h;
+    input [7:0] sa;
+    output [15:0] avg;
+    
+    wire [31:0] l00, l01, l02, l03, l10, l11, l2, l2div2, l2div4, l2div8;
+    
+    ADD #(.DATAWIDTH(32)) ADD1(a,b,l00);
+    ADD #(.DATAWIDTH(32)) ADD2(c,d,l01);
+    ADD #(.DATAWIDTH(32)) ADD3(e,f,l02);
+    ADD #(.DATAWIDTH(32)) ADD4(g,h,l03);
+    ADD #(.DATAWIDTH(32)) ADD5(l00,l01,l10);
+    ADD #(.DATAWIDTH(32)) ADD6(l02,l03,l11);
+    ADD #(.DATAWIDTH(32)) ADD7(l02,l11,l2);
+    SHR #(.DATAWIDTH(32)) SHR1(l2,l2div2,{8'b0,sa});
+    SHR #(.DATAWIDTH(32)) SHR2(l2div2,l2div4,{8'b0,sa});
+    SHR #(.DATAWIDTH(32)) SHR3(l2div4,l2div8,{8'b0,sa});
+    REG #(.DATAWIDTH(32)) REG1(l2div8, Clk, Rst, avg);
 endmodule
