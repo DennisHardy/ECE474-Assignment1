81e8c5f4cbce9b081ba5ac4477b8056c157a05f7
Dennis Hardy <DennisHardy@email.arizona.edu>
2018-09-25 19:35:51 -0700

    Circuit 1 fixed for latency calc

--- Behavioral netlists/circuit1.v
+++ Behavioral netlists/circuit1.v
@@ -5,22 +5,22 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module Circuit1 (a,b,c,z,x);
+module Circuit1 (a,b,c,z,x, Clk, Rst);
+    input Clk, Rst;
     input [7:0] a,b,c;
-    output reg [7:0] z;
-    output reg [15:0] z;
-    wire [8:0] d,e;
+    output [7:0] z;
+    output [15:0] x;
+    wire [7:0] d,e;
     wire [15:0] f,g;
     wire [15:0] xwire;
 
-    ADD ADD1(a,b,d);
-    ADD ADD2(a,c,e);
-    COMP COMP1(d,e,g,,);
-    MUX2x1 MUX1(d,e,z,g);
-    MUL MUL1 (a,c,f);
-    SUB SUB1 (f,d,xwire);
+    ADD #(.DATAWIDTH(8)) ADD1(a,b,d);
+    ADD #(.DATAWIDTH(8)) ADD2(a,c,e);
+    COMP #(.DATAWIDTH(8)) COMP1(d,e,g[0],,);
+    MUX2x1 #(.DATAWIDTH(8)) MUX1(d,e,g[0],z);
+    MUL #(.DATAWIDTH(8)) MUL1 (a,c,f);
+    SUB #(.DATAWIDTH(16)) SUB1 (f,{8'b0,d},xwire);
+    REG #(.DATAWIDTH(16)) REG1 (xwire, Clk, Rst, x);
     
-    always @(xwire) begin
-    x <= xwire;
-    end
+
 endmodule
